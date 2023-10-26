1c7a3e5623f3812972022c690f2773618aca0a51
DwAwilliams23 <120130350+DwAwilliams23@users.noreply.github.com>
2023-10-26 09:52:18 -0400

    Fixed create and delete functionality for all ingredient list items

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -3,12 +3,41 @@ const Ingredient = require('../models/recipe')
 
 module.exports = {
     create,
+    createGroupIngredient,
     edit,
     update,
     delete: deleteIngredient
 }
 
 function create(req, res) {
+    Recipe.findOne({ _id: req.params.id }, function (err, recipe) {
+        if (err || !recipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+
+        // Create the ingredient object and add it to the main recipe's ingredients array
+        const ingredient = {
+            user: req.user._id,
+            userName: req.user.name,
+            userAvatar: req.user.avatar,
+            quantity: req.body.quantity,
+            measurement: req.body.measurement,
+            content: req.body.content,
+        };
+
+        recipe.ingredients.push(ingredient);
+
+        recipe.save(function (err) {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to add ingredient to recipe' });
+            }
+            res.redirect(`/recipes/${recipe._id}`);
+        });
+    });
+}
+
+function createGroupIngredient(req, res) {
     Recipe.findOne({ _id: req.params.id, 'groups._id': req.params.groupId }, function (err, recipe) {
         if (err || !recipe) {
             return res.status(404).json({ message: 'Recipe or Group not found' });
@@ -72,12 +101,34 @@ function update(req, res) {
   
 async function deleteIngredient(req, res, next) {
     try {
-        const recipe = await Recipe.findOne({'ingredients._id': req.params.id})
-        if (!recipe) return res.redirect('/recipes')
-        recipe.ingredients.remove(req.params.id)
-        await recipe.save()
-        res.redirect(`/recipes/${recipe._id}`)
-    } catch(err) {
-        return next(err)
+        // Attempt to find the recipe
+        const recipe = await Recipe.findOne({ _id: req.params.id });
+
+        if (!recipe) {
+            return res.redirect('/recipes');
+        }
+
+        // Check if the ingredient is in the main recipe's ingredient list
+        let ingredient = recipe.ingredients.id(req.params.ingredientId);
+
+        if (!ingredient) {
+            // If the ingredient is not in the main recipe's list, search within groups
+            for (const group of recipe.groups) {
+                ingredient = group.ingredients.id(req.params.ingredientId);
+                if (ingredient) {
+                    // Remove the ingredient from the group
+                    group.ingredients.remove(ingredient);
+                    break; // Exit the loop once found
+                }
+            }
+        } else {
+            // Remove the ingredient from the main recipe
+            recipe.ingredients.remove(ingredient);
+        }
+
+        await recipe.save();
+        res.redirect(`/recipes/${recipe._id}`);
+    } catch (err) {
+        return next(err);
     }
 }
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -8,8 +8,8 @@ router.post('/recipes/:id/ingredients', ensureLoggedIn, ingredientsCtrl.create)
 router.get('/recipes/:id/ingredients/:ingredientId/edit', ensureLoggedIn, ingredientsCtrl.edit);
 router.put('/recipes/:id/ingredients/:ingredientId', ensureLoggedIn, ingredientsCtrl.update);
 router.delete('/ingredients/:id', ensureLoggedIn, ingredientsCtrl.delete)
-router.post('/recipes/:id/groups/:groupId/ingredients', ensureLoggedIn, ingredientsCtrl.create);
-router.get('/recipes/:id/groups/:groupId/ingredients/:ingredientId/edit', ensureLoggedIn, ingredientsCtrl.edit);
+router.post('/recipes/:id/groups/:groupId/ingredients', ensureLoggedIn, ingredientsCtrl.createGroupIngredient);
+router.get('/recipes/:id/groups/:id/ingredients/:id/edit', ensureLoggedIn, ingredientsCtrl.edit);
 router.put('/recipes/:id/groups/:groupId/ingredients/:ingredientId', ensureLoggedIn, ingredientsCtrl.update);
 router.delete('/recipes/:id/groups/:groupId/ingredients/:ingredientId', ensureLoggedIn, ingredientsCtrl.delete);
 
