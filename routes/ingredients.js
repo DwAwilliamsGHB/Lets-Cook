01c3d646a5e5733069b9e6e21e6f78c3610e7f02
DwAwilliams23 <120130350+DwAwilliams23@users.noreply.github.com>
2023-10-31 23:28:57 -0400

    Fixing CRUD for ingredients on the main recipe list

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -7,7 +7,7 @@ const ensureLoggedIn = require('../config/ensureLoggedIn')
 router.post('/recipes/:id/ingredients', ensureLoggedIn, ingredientsCtrl.create)
 router.get('/recipes/:id/ingredients/:ingredientId/edit', ensureLoggedIn, ingredientsCtrl.edit);
 router.put('/recipes/:id/ingredients/:ingredientId', ensureLoggedIn, ingredientsCtrl.update);
-router.delete('/ingredients/:id', ensureLoggedIn, ingredientsCtrl.delete)
+router.delete('/recipes/:id/ingredients/:ingredientId', ensureLoggedIn, ingredientsCtrl.delete)
 router.post('/recipes/:id/groups/:groupId/ingredients', ensureLoggedIn, ingredientsCtrl.createGroupIngredient);
 router.get('/recipes/:id/groups/:groupId/ingredients/:ingredientId/groupIngredientEdit', ensureLoggedIn, ingredientsCtrl.editGroupIngredient);
 router.put('/recipes/:id/groups/:groupId/ingredients/:ingredientId', ensureLoggedIn, ingredientsCtrl.updateGroupIngredient);
